2e71138632aab2a81ae4bd40fd75d6e5cbafc8ca
RAMPRADOP S <119684694+rampradops28@users.noreply.github.com>
2025-01-06 15:39:00 +0530

    Update Createuser.jsx

--- src/Createuser.jsx
+++ src/Createuser.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import axios from 'axios'
 import { useNavigate } from 'react-router-dom';
+import './Createuser.css';
 const Createuser = () => {
 
   const [ name, setName] = useState('');
